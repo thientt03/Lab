e393eb5bb8db84fae8c93696546459b2d353cfeb
Trần khánh thiện <47145219+thientt03@users.noreply.github.com>
2019-02-22 22:25:51 +0700

    Update test.py

--- test.py
+++ test.py
@@ -8,13 +8,18 @@ conn = urlopen(url)
 raw_data = conn.read()
 #decode data
 content = raw_data.decode("utf8")
-#mở-ghi file tải về
+#mở-ghi-đóng file tải về
 with open("sua.html", "wb") as f:
     f.write(raw_data)
+    f.close()
+print(f)
 #find ROI
 soup = BeautifulSoup(content, "html.parser")
-ul_new = soup.find("b_r_c", style="width:32%")
-print(ul_new)
+ul_new = soup.find("div", style="overflow:hidden;width:100%;border-bottom:solid 1px #cecece;")
+li_list = ul_new.find_all("tr")
 
+# print(li_list)
+# for td in li_list:
+n = li_list[0]
 
-
+print(n)
